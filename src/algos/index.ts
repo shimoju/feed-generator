b7b24d81f833e848ce23580654097904b4b52752
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2023-07-08 15:40:34 +0900

    ハンドラの設定

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,12 +3,12 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './whats-alf'
+import * as genshin from './genshin'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
+  [genshin.shortname]: genshin.handler,
 }
 
 export default algos
